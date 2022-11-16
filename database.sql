f1b94c76a1d04176d4b25ae353e63f524bcd7b9e
Fan-Lo <fanny.lo@ucalgary.ca>
2022-11-16 09:14:19 -0700

    created database.sql

---
+++ database.sql
@@ -0,0 +1,7 @@
+# Host: localhost    Database: 471
+# ------------------------------------------------------
+# Server version 5.0.51b-community-nt-log
+
+DROP DATABASE IF EXISTS `471`;
+CREATE DATABASE `471` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `471`;
