40d066684e7bcc725bf622ab0e58ab680831d25a
lizzy-io <117678027+lizzy-io@users.noreply.github.com>
2022-11-27 10:41:28 -0700

    Update and rename Gov,Inv,Ser,Pro.py to Gov,Inv,Ser,Pro,Ins.py

--- Gov,Inv,Ser,Pro,Ins.py
+++ Gov,Inv,Ser,Pro,Ins.py
@@ -52,3 +52,14 @@ class Service:
 
     def getCost(self):
         return self.cost
+    
+class Insurance:
+    def __init__(self, MEMBERID, POLICYNO):
+        self.MEMBERID = MEMBERID
+        self.POLICYNO = POLICYNO
+
+    def getPolicyNO(self):
+        return self.POLICYNO
+
+    def getMemberID(self):
+        return self.MEMBERID
