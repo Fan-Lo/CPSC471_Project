d1e3edbc267f8086ca502885d8bd114dc0becd79
Sobia-Khan1 <101202543+Sobia-Khan1@users.noreply.github.com>
2022-11-28 09:19:16 -0700

    Add files via upload

---
+++ DatabaseConnect.py
@@ -0,0 +1,37 @@
+import mysql.connector 
+
+class DatabaseConnect:
+
+    def __init__(self):
+        p = input("Enter Database Password: ")
+        u = input("Enter Database Username: ")
+
+        try:
+            self.connection = mysql.connector.connect(user = u, password = p, host='127.0.0.1', database='471')
+
+        except mysql.connector.Error as error:
+            print("Failed to create table in MySQL: {}".format(error))
+
+    def getTable(self, s, table):
+        mycursor = self.connection.cursor()
+
+        mycursor.execute(f"SELECT {s} FROM {table}")
+
+        myresult = mycursor.fetchall()
+        return myresult
+
+    def performQuery(self, query):
+        mycursor = self.connection.cursor()
+
+        mycursor.execute(query)
+
+        myresult = mycursor.fetchall()
+        return myresult
+
+    def close(self):
+        if self.connection.is_connected():
+            self.connection.close()
+
+if __name__ == "__main__":
+    database = DatabaseConnect()
+    database.close()
