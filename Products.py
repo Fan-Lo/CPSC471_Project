f4483353e65e7456bef854a37c8e02cccb8e2285
lizzy-io <117678027+lizzy-io@users.noreply.github.com>
2022-11-27 21:40:23 -0700

    Update Products.py

--- Products.py
+++ Products.py
@@ -1,25 +1,25 @@
 
 class Products:
     def __init__(self, names, Supplier, cost, amountInStock):
-        self.names = names
-        self.Supplier = Supplier
-        self.cost = cost
-        self.amountInStock = amountInStock
+        self.__names = names
+        self.__Supplier = Supplier
+        self.__cost = cost
+        self.__amountInStock = amountInStock
 
     def getName(self):
-        return self.names
+        return self.__names
     
     def getSupplier(self):
-        return self.Supplier
+        return self.__Supplier
 
     def setSupplier(self, s):
-        self.Supplier = s
+        self.__Supplier = s
 
     def getCost(self):
-        return self.cost
+        return self.__cost
 
     def setCost(self, c):
-        self.cost = c
+        self.__cost = c
 
     def getAmountInStock(self):
-        return self.amountInStock
+        return self.__amountInStock
