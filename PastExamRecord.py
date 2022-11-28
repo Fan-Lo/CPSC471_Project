fc2446e9798b0cdfcb52b465b24d5a279e5f8799
Fan-Lo <fanny.lo@ucalgary.ca>
2022-11-27 20:48:34 -0700

    PastExamRecord.py

--- PastExamRecord.py
+++ PastExamRecord.py
@@ -1,6 +1,13 @@
 from ExamDetail import ExamDetail
 
 class PastExamRecord:
-    detail = []
 
+    def __init__(self):
+        self.__detail = []
+
+
+    def getExamDetail(self, r):
+        return self.__detail[r]
     
+    def addExamDetail(self, examID):
+        self.__detail.insert(examID, ExamDetail())
