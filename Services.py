5fa35fad5f214fdf93342995883069e9436ed7ae
lizzy-io <117678027+lizzy-io@users.noreply.github.com>
2022-11-27 21:06:15 -0700

    Create Services.py

---
+++ Services.py
@@ -0,0 +1,10 @@
+class Service:
+    def __init__(self, name, cost):
+        self.name = name
+        self.cost = cost
+
+    def getName(self):
+        return self.name
+
+    def getCost(self):
+        return self.cost
