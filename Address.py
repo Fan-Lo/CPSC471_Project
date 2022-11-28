2d0df377aed5133252694af33bbc6a71682a83c9
Fan-Lo <fanny.lo@ucalgary.ca>
2022-11-28 09:38:19 -0700

    parseAddress() completed and tested

--- Address.py
+++ Address.py
@@ -1,5 +1,5 @@
 class Address:
-    def __init__(self, country, province, city, address, posCode):
+    def __init__(self, country=None, province=None, city=None, address=None, posCode=None):
         self.__country = country
         self.__province = province
         self.__city = city
@@ -20,3 +20,32 @@ class Address:
         
     def setPostalCode(self, posCode):
         self.__posCode = posCode
+    
+    def parseAddress(self, address):
+        addressList = address.split(',')
+        self.__address = addressList[0].strip()
+        self.__city = addressList[1].strip()
+        self.__province = addressList[2].strip()
+        self.__country = addressList[3].strip()
+    
+    def getAddress(self):
+        return self.__address
+    
+    def getCountry(self):
+        return self.__country
+    
+    def getCity(self):
+        return self.__city
+    
+    def getProvince(self):
+        return self.__province
+
+# Testing parseAddress()  
+'''
+if __name__ == '__main__':
+    add = Address()
+    add.parseAddress("149 Sage Meadows Cir NW, Calgary, Alberta, Canada")
+    print(add.getAddress())
+    print(add.getCountry())
+'''  
+
