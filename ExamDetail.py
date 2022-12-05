c8d559cdbfdc7163ff544938f06219bb95472d34
lizzy-io <117678027+lizzy-io@users.noreply.github.com>
2022-12-05 11:02:44 -0700

    all patient buttons working

--- ExamDetail.py
+++ ExamDetail.py
@@ -1,12 +1,10 @@
+from DatabaseConnect import *
+
 class ExamDetail:
     __performedBy = []
 
-    def __init__(self, id, d, notes, m, r=False):
-        self.__examID = id
-        self.__notes = notes
-        self.__performedBy = m
-        self.__date = d
-        self.__referral = r
+    def __init__(self):
+        pass
 
     def getExamID(self):
         return self.__examID
@@ -25,3 +23,24 @@ class ExamDetail:
     
     def setPerformedBy(self, m):
         self.__performedBy = m
+
+    def viewExamDetail(self, AHC):
+        self.database = DatabaseConnect()
+        self.exam_details = self.database.performQuery(f"SELECT * FROM EXAM_DETAIL WHERE PatAHC = '{AHC}';")
+        self.database.close()
+        if(len(self.exam_details) == 0):
+            return 'You have no exam details to display'
+
+        i = 0
+        format = ''
+        while (i < len(self.exam_details)):
+            num = i+1
+            format += ' Exam Detail ' + str(num) + ': \n'
+            format += '     ' + 'Exam ID: ' + str(self.exam_details[i][0]) + '\n'
+            format += '     ' + 'Patient AHC: ' + str(self.exam_details[i][1]) + '\n'
+            dt = self.exam_details[i][2].strftime('%Y-%m-%d')
+            format += '     ' + 'Date' + dt + '\n'
+            format += '     ' + 'Notes: ' + str(self.exam_details[i][3]) + '\n'
+            format += '     ' + 'Employee SIN: ' + str(self.exam_details[i][4]) + '\n' 
+            i += 1
+        return format
